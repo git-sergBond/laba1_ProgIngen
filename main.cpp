2dea8f1852355550ead022b277223259c219fd7f
git-sergBond <git-serg.bond@yandex.ru>
2018-02-21 22:03:56 +0300

    proga dlya dvux chisel

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
-
-using namespace std;
+#include "sdt.h"
 
 int main()
 {
-    cout << "Hello wor1ld!" << endl;
+    int16_t a=0, b=0;
+
+    cout << "please, type value of A variable here: ";
+    cin >> a;
+
+    cout << endl << "please, type value of B variable here: ";
+    cin >> b;
+
+    cout << endl << "A = " << a << ", B = " << b << endl;
+
+    _getch();
     return 0;
 }
