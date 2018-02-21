22817a0172d9e756d4e6d3e1df5fee5b9c7e057f
git-sergBond <git-serg.bond@yandex.ru>
2018-02-21 22:18:14 +0300

    dobavil sloshenie chisel

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
     cout << endl << "please, type value of B variable here: ";
     cin >> b;
 
-    cout << endl << "A = " << a << ", B = " << b << endl;
+    cout << endl << "A = " << a << ", B = " << b << endl
+        << "A + B = " << (a+b) << endl;
 
     _getch();
     return 0;
