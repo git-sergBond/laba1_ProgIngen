fc99b021de2149b4c056dddd7eecb2505017c1df
git-sergBond <git-serg.bond@yandex.ru>
2018-02-25 18:03:10 +0300

    super commit branch named func

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ int main()
 {
     int16_t a=0, b=0;
 
-    cout << "please, type value of A variable here: ";
+    cout << "please, type value of A variable here: ";//func
     cin >> a;
 
     cout << endl << "please, type value of B variable here: ";
     cin >> b;
 
     cout << endl << "A = " << a << ", B = " << b << endl
-        << "A + B = " << (a+b) << endl;
+        << "A + B = " << (a+b) << endl;//func
 
     cout << "A - B = " << (a-b) << endl;
 	cout << "A * B = " << a * b << '\n';
-    cout << "A / B = " << (a/b) << endl;
+    cout << "A / B = " << (a/b) << endl;//func
     _getch();
     return 0;
 }
